e63a7b0f4f14856db899c7f3c43211f1fbe252d5
Asis Rai <asis.rai01@gmail.com>
2022-07-07 18:51:53 +0800

    few minor changes and updated readme.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # asisrai2207.github.io
+# My Portfolio Page
+
+I have created a potfolio for my Individual project and also to showcase my skills and Portfolio. To achieve this i use the following technology:
+
+### HTML
+### CSS
+### Bootstrap
+### Jquery
+### Javascript 
+### Few Libraries such as Animate.css and Wow.js
+
+The website is still work on progress, will add more features as time goes by.
--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,3 @@
-// var scroll = new SmoothScroll('a[href*="#"]', {
-//     speed: 1000,
-//     //   speedAsDuration: true
-//   });
-
-// const navBar = document.getElementsByClassName("navbar")[0];
-
-// console.log(document.getElementsByClassName("navbar"));
-
-// window.addEventListener("scroll", function () {
-//   console.log(window.scrollY);
-
-//   if (window.scrollY > 100) {
-//     navBar.classList.add("navbarInverted");
-//     navBar.classList.remove("bg-light");
-//   } else if (window.scrollY <= 99) {
-//     navBar.classList.add("bg-light");
-//     navBar.classList.remove("navbarInverted");
-//   }
-// });
 
 $('input[placeholder="Your Name Here"]').on("keyup", function (e) {
   var input = e.target.value;
